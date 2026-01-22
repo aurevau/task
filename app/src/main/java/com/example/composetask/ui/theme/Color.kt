1419ca7640fd67ea1166115c24cf14b61f4371ef
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-01-22 20:29:01 +0100

    added colors and fonts

---
+++ app/src/main/java/com/example/composetask/ui/theme/Color.kt
@@ -0,0 +1,21 @@
+package com.example.composetask.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val black = Color(0x121212)
+val orange = Color(0xFFA040)
+val lightGray = Color(0xD9D9D9)
+val gray = Color(0x2C2C2C)
+val red = Color(0xFF3040)
+val white = Color(0xFFFFFF)
+val mediumBlue = Color(0x0095F6)
+val blue = Color(0x005BB5)
+val whiteVariant = Color(0xFEFFF7)
