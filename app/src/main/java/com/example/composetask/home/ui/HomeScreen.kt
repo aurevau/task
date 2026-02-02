6d3d544345499b8fa99aaceefaf147c7b7ed8b0f
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-02-02 12:02:48 +0100

    fixed design on chats layout

--- app/src/main/java/com/example/composetask/home/ui/HomeScreen.kt
+++ app/src/main/java/com/example/composetask/home/ui/HomeScreen.kt
@@ -5,20 +5,28 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -27,9 +35,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.example.composetask.R
 import com.example.composetask.home.HomeViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -49,7 +64,7 @@ fun HomeScreen(navController: NavHostController){
                 .padding(16.dp)
                 .clip(RoundedCornerShape(16.dp))
                 .background(MaterialTheme.colorScheme.onBackground)
-                .clickable{
+                .clickable {
                     addChannel.value = true
                 }) {
                 Text(text = "Add Channel",
@@ -57,24 +72,44 @@ fun HomeScreen(navController: NavHostController){
                         .padding(16.dp),
                     color = MaterialTheme.colorScheme.background)
             }
-        }
+        }, containerColor = MaterialTheme.colorScheme.background
     ) {
-        Box(modifier = Modifier.padding(it)
+        Box(modifier = Modifier
+            .padding(it)
             .fillMaxSize()) {
             LazyColumn() {
+                item{
+                    Text(text = stringResource(R.string.messages), color = MaterialTheme.colorScheme.onBackground, style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Black), modifier = Modifier.padding(16.dp ))
+                }
+
+                item {
+                    TextField(modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 8.dp)
+                        .clip(RoundedCornerShape(40.dp)),value = "", onValueChange = {}, placeholder = {Text(text = "Search..", color = MaterialTheme.colorScheme.onBackground)},
+                      textStyle = TextStyle(color = MaterialTheme.colorScheme.onBackground),
+                        colors = TextFieldDefaults.colors().copy(focusedContainerColor = MaterialTheme.colorScheme.background, unfocusedContainerColor = MaterialTheme.colorScheme.background ),
+                        trailingIcon = { Icon(imageVector = Icons.Filled.Search, contentDescription = "Search", tint = MaterialTheme.colorScheme.onBackground) }
+                    )
+
+                }
                 items(channels.value) { channel ->
                     Column() {
-                        Text(modifier = Modifier.fillMaxWidth()
-                            .padding(8.dp)
-                            .clip(RoundedCornerShape(16.dp))
-                            .background(MaterialTheme.colorScheme.onBackground)
-                            .clickable{
-                                navController.navigate("chat/${channel.id}")
-                            }
-                            .padding(16.dp)
-                            ,
-                            color = MaterialTheme.colorScheme.background
-                            ,text = channel.name)
+                        ChannelItem(channel.name, {
+                            navController.navigate("chat/${channel.id}")
+                        })
+//                        Text(modifier = Modifier
+//                            .fillMaxWidth()
+//                            .padding(8.dp)
+//                            .clip(RoundedCornerShape(16.dp))
+//                            .background(MaterialTheme.colorScheme.onBackground)
+//                            .clickable {
+//
+//                            }
+//                            .padding(16.dp)
+//                            ,
+//                            color = MaterialTheme.colorScheme.background
+//                            ,text = channel.name)
                     }
                 }
             }
@@ -91,6 +126,35 @@ fun HomeScreen(navController: NavHostController){
                 addChannel.value = false
             }
         }
+    }
+}
+
+@Composable
+fun ChannelItem(channelName: String, onClick: () -> Unit) {
+    Row(modifier = Modifier.fillMaxWidth()
+        .padding(horizontal = 16.dp, vertical = 2.dp)
+        .clip(RoundedCornerShape(24.dp))
+        .background(MaterialTheme.colorScheme.onBackground)
+        .clickable{
+            onClick()
+        }
+        ,
+        verticalAlignment = Alignment.CenterVertically) {
+        Box(modifier = Modifier.padding(12.dp)
+            .size(60.dp)
+            .clip(CircleShape)
+            .background(MaterialTheme.colorScheme.tertiary.copy(alpha = 0.3f))
+            ) {
+
+            Text(text = channelName[0].uppercase(),
+                color = MaterialTheme.colorScheme.background,
+                style = TextStyle(fontSize = 35.sp),
+                textAlign = TextAlign.Center,
+                modifier = Modifier.align (Alignment.Center))
+        }
+
+
+        Text(text = channelName.uppercase(), modifier = Modifier.padding(8.dp), color = MaterialTheme.colorScheme.background, fontWeight = FontWeight.SemiBold)
     }
 }
 
