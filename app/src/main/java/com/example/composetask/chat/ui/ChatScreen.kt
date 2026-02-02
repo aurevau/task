212e26f5489de1432133a5a1714e69e8afaf487f
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-02-02 12:21:15 +0100

    fixed design to chatbubbles and chatbackground

--- app/src/main/java/com/example/composetask/chat/ui/ChatScreen.kt
+++ app/src/main/java/com/example/composetask/chat/ui/ChatScreen.kt
@@ -1,12 +1,17 @@
 package com.example.composetask.chat.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -16,6 +21,8 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Send
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -27,28 +34,36 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.composetask.R
 import com.example.composetask.chat.ChatViewModel
 import com.example.composetask.chat.model.Message
 import com.google.firebase.auth.FirebaseAuth
 
 @Composable
 fun ChatScreen(navController: NavController, channelId: String) {
-    val viewModel: ChatViewModel = hiltViewModel()
-    LaunchedEffect(key1 = true) {
-        viewModel.listenForMessages(channelId)
+    Scaffold(containerColor = MaterialTheme.colorScheme.background) {
+        Column(modifier = Modifier.fillMaxSize().padding(it)) {
+            val viewModel: ChatViewModel = hiltViewModel()
+            LaunchedEffect(key1 = true) {
+                viewModel.listenForMessages(channelId)
+            }
+
+            val messages = viewModel.messages.collectAsState()
+            ChatMessages(
+                messages = messages.value,
+                onSendMessage = {message ->
+                    viewModel.sendMessage(channelId, message)
+                }
+            )
+
+        }
     }
 
-    val messages = viewModel.messages.collectAsState()
-    ChatMessages(
-        messages = messages.value,
-        onSendMessage = {message ->
-            viewModel.sendMessage(channelId, message)
-        }
-    )
 
 
 
@@ -102,22 +117,34 @@ fun ChatBubble(message: Message) {
     val isCurrentUser = message.senderId == FirebaseAuth.getInstance().currentUser?.uid
 
     val bubbleColor = if(isCurrentUser) {
-        Color.Blue
-    } else {Color.Green}
+        MaterialTheme.colorScheme.onBackground
+    } else {
+        MaterialTheme.colorScheme.tertiary
+    }
 
     Box(modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp, horizontal = 8.dp))
          {
 
         val alignment = if(!isCurrentUser) Alignment.CenterStart else Alignment.CenterEnd
 
-            Box(
+            Row(
                 modifier = Modifier
                 .padding(8.dp)
-                    .align(alignment)
-                .background(color = bubbleColor, shape = RoundedCornerShape(8.dp))) {
-                Text(text = message.message,
+                    .align(alignment),
+                verticalAlignment = Alignment.CenterVertically
+                ) {
+
+                if(!isCurrentUser) {
+                    Image(painter = painterResource(id = R.drawable.friend), contentDescription = "Image Friend", modifier = Modifier.size(40.dp))
+
+                    Spacer(modifier = Modifier.width(8.dp))
+                }
+
+
+                Text(text = message.message.trim(),
                     color = Color.White,
-                    modifier = Modifier.padding(8.dp))
+                    modifier = Modifier
+                        .background(color = bubbleColor, shape = RoundedCornerShape(10.dp)).padding(10.dp))
 
 
             }
